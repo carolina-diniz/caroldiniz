1512fc5daf99d997f4f6e848c8f79f3ce94446d9
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-27 09:26:03 -0300

    Adiciona rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreMimComponent } from './pages/sobre-mim/sobre-mim.component';
+import { PortifolioComponent } from './pages/portifolio/portifolio.component';
+import { HabilidadesComponent } from './pages/habilidades/habilidades.component';
+import { FormacaoComponent } from './pages/formacao/formacao.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'portifolio', pathMatch: 'full'},
+  {path: 'portifolio', component: PortifolioComponent},
+  {path: 'sobre-mim', component: SobreMimComponent},
+  {path: 'habilidades', component: HabilidadesComponent},
+  {path: 'formacoes', component: FormacaoComponent},
+  {path: 'contato', component: ContatoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
