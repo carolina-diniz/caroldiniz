f447fa76946917f37fc9d36e36484caa77cd4be7
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-27 09:26:28 -0300

    adiciona modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SobreMimComponent } from './pages/sobre-mim/sobre-mim.component';
+import { PortifolioComponent } from './pages/portifolio/portifolio.component';
+import { CardPortifolioComponent } from './core/components/card-portifolio/card-portifolio.component';
+import { HabilidadesComponent } from './pages/habilidades/habilidades.component';
+import { FormacaoComponent } from './pages/formacao/formacao.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { CursosComponent } from './core/components/cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreMimComponent,
+    PortifolioComponent,
+    CardPortifolioComponent,
+    HabilidadesComponent,
+    FormacaoComponent,
+    ContatoComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
